29bed44e21f51554d6506622f2d47ffbd2bb7ad4
mohamadrizki <mohamadrizkimaghfaluti@gmail.com>
2023-01-06 10:39:59 +0700

    Add new project 'ticket-reservations'

---
+++ SpringBoot/ticket-reservations/src/main/java/com/java/ticketreservations/TicketReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.java.ticketreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketReservationsApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/ticket-reservations/src/test/java/com/java/ticketreservations/TicketReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ticketreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
